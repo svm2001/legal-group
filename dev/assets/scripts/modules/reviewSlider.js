b04e7368ef45845f3cd77d99e86c1885f285995f
Вячеслав Сидоров <v.sidorov@intaro.email>
2025-05-02 19:30:37 +0400

    fix

---
+++ dev/assets/scripts/modules/reviewSlider.js
@@ -0,0 +1,80 @@
+import { Swiper } from '../../../../node_modules/swiper/swiper-bundle.min.mjs'
+import 'swiper/css'
+
+export function reviewSlider() {
+  const slider = document.querySelector('.reviews__slider.swiper')
+
+  if (!slider) return
+
+  const swiper = new Swiper(slider, {
+    slidesPerView: 'auto',
+    slidesPerGroup: 1,
+    centeredSlides: false,
+    freeMode: true,
+    loop: false,
+    initialSlide: 0,
+    spaceBetween: 12,
+    slideToClickedSlide: true,
+    autoHeight: false,
+    draggable: true,
+    speed: 300,
+    navigation: {
+      nextEl: '.reviews__slider-arrows-item--next',
+      prevEl: '.reviews__slider-arrows-item--prev'
+    },
+    breakpoints: {
+      1200: {
+        freeMode: false,
+      }
+    },
+    on: {
+      init: function () {
+        if(window.innerWidth > 1199) {
+          updateSlideStyles(this);
+        }
+      },
+      slideChange: function () {
+        if(window.innerWidth > 1199) {
+          updateSlideStyles(this);
+        }
+      }
+    }
+  })
+
+  function updateSlideStyles(swiper) {
+    swiper.slides.forEach((slide, index) => {
+      if (index > swiper.activeIndex + 2) {
+        slide.classList.add('slide-dimmed');
+      } else {
+        slide.classList.remove('slide-dimmed');
+      }
+    });
+  }
+
+  const container = document.querySelector('.reviews .container')
+  const plug = document.querySelector('.reviews .plug')
+  if(window.innerWidth >= 1368) {
+    plug.style.width = ((document.documentElement.clientWidth - container.offsetWidth) / 2) + 252 + `px`
+  }
+   else {
+    plug.style.width = ((document.documentElement.clientWidth - container.offsetWidth) / 2) + 242 + `px`
+  }
+
+  if(window.innerWidth > 1199) {
+    const counterValue = document.querySelector('.reviews__slider-counter-value')
+    const counterSum = document.querySelector('.reviews__slider-counter-sum')
+
+    if (counterValue && counterSum) {
+      counterSum.textContent = String(swiper.slides.length).padStart(2, '0')
+      counterValue.textContent = String(swiper.realIndex + 1).padStart(2, '0')
+
+      swiper.on('slideChange', () => {
+        setTimeout(() => {
+          counterValue.textContent = String(swiper.realIndex + 1).padStart(2, '0')
+        }, 320)
+      })
+    }
+  } else  {
+    if(document.querySelector('.reviews__slider-info')) document.querySelector('.reviews__slider-info').remove()
+  }
+}
--- dev/pages/index/index.js
+++ dev/pages/index/index.js
@@ -1,11 +1,33 @@
 import './index.scss'
 import main from '@/assets/scripts/main'
+import tabs from "@/assets/scripts/modules/tabs.js"
+import { reviewSlider } from "@/assets/scripts/modules/reviewSlider.js"
 window.onload = () => {
   main()
+  reviewSlider()
+  const services = document.querySelector('.services')
 
   document.querySelectorAll('.js-amo-button').forEach(button => {
     button.addEventListener('click', () => {
-      window.kronosOnlineForm.modal.show()
+      const isInServicesCard = button.closest('.services-card')
+      if (isInServicesCard) {
+        window.hystModal.close()
+        setTimeout(() => window.kronosOnlineForm.modal.show(), 300)
+      } else window.kronosOnlineForm.modal.show()
     })
   })
+
+  if(services) {
+    const servicesModalCustomBtnClose = services.querySelectorAll('.services-card__modal-bottom-close')
+    servicesModalCustomBtnClose.forEach(button => button.addEventListener('click', () => window.hystModal.close()))
+
+    tabs({
+      tabParentSelector: '.services',
+      tabHeaderSelector: '.services__tabs-item',
+      tabHeaderActiveClass: 'active',
+      tabItemSelector: '.services__content',
+      tabItemActiveClass: 'active',
+      event: 'click',
+    })
+  }
 }
